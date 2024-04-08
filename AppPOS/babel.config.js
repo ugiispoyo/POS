14fcd57e9edff85620ad474540727187c5497172
Ugi Ispoyo Widodo <ugi.widodo@kitabisa.com>
2024-04-09 00:57:50 +0700

    fix setup for release apk

--- AppPOS/babel.config.js
+++ AppPOS/babel.config.js
@@ -1,17 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          tests: ['./tests/'],
-          '@components': './src/components',
-        },
-      },
-    ],
-    'react-native-reanimated/plugin',
-  ],
+    'react-native-reanimated/plugin'
+  ]
 };
